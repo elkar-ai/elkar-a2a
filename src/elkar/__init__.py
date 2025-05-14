8772fcdea2c25b1bedec22c4113410f8b3bbd799
MEUNIER Laurent <laurent@breezyai.co>
2025-05-14 16:21:03 +0200

    feat: bump version worflow

--- src/elkar/__init__.py
+++ src/elkar/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.1.12"
+__version__ = "0.1.10"
 
 
 from . import a2a_types, server, store, task_manager
 
-__all__ = ["a2a_types", "server", "task_manager", "store"]
+__all__: list[str] = ["a2a_types", "server", "task_manager", "store"]
--- src/elkar/main.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-import uvicorn
-
-# Run both API and A2A server
-if __name__ == "__main__":
-    uvicorn.run("app_sample:app", host="0.0.0.0", port=5001, reload=True)
